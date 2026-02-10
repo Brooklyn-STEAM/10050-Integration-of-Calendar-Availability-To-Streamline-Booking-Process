bfa704a98dd43f6027d78977fd2e12ee26811d12
Sanah White <sanahwhite7@gmail.com>
2026-02-10 08:57:39 -0500

    some changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect
-
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 import pymysql
 
 from dynaconf import Dynaconf
@@ -14,3 +14,14 @@ app.secret_key = config.secret_key
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+@app.route("/docor.html.jinja")
+def search():
+    connection = connect_db()
+
+    cursor = connection.cursor
+
+    cursor.execute("SELECT * FROM `Doctor`")
+
+    connection.close()
+    return render_template("docor.html.jinja")
