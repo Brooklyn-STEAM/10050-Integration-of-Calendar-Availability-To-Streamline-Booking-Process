8c03a17334107415e93f75afd90df63a083e5ab5
Sanah White <sanahwhite7@gmail.com>
2026-02-12 09:36:07 -0500

    add,listingdoctors

--- main.py
+++ main.py
@@ -69,25 +69,73 @@ def docor():
 
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM `DoctorAvailability` ")
+    cursor.execute("SELECT * FROM `Doctor` ")
 
     result = cursor.fetchall()
 
     connection.close()
-    return render_template("docor.html.jinja")
+    return render_template("docor.html.jinja",doc =result )
+
+@app.route("/docor/<Doctor_id>")
+def doctor_page(Doctor_id):
+    connection = connect_db()
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * `Doctor` WHERE `ID` = %s", (Doctor_id))
+
+    result = cursor.fetchone()
+
+    cursor.execute("SELECT * FROM ``Review` JOIN `User` ON `User`. `ID` = `Review` . `userID` `DoctorID` = %s", (Doctor_id))
+
+    result2 = cursor.fetchall()
+
+    connection.close()
+
+    if result is None:
+        abort(404)
+
+    return render_template("docor.html.jinja", doctor = result, review = result2)
+
+    
+
+
+
+
+
+
+
+
+@app.route ("/appoint/<Doctor_id>/add_to_list", methods=["POST"])
+@login_required
+def add_to_list(Doctor_id):
+    time = request.form[]
+
+    
+
+
+
+
 
 @app.route("/appoint")
+@login_required
 def appoint():
     connection = connect_db()
 
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM `Appointment` ")
-
+    cursor.execute("""
+    SELECT * FROM `Appointment`
+    JOIN `Doctor` ON `Doctor`. `ID` = `Appointment`. `DoctorID`
+    WHERE `UserID` = %s
+    """, (current_user.id))
     result = cursor.fetchall()
 
     connection.close()
-    return render_template("appoint.html.jinja")
+    return render_template("appoint.html.jinja", appointment = result)
+
+
+
 
 @app.route("/doctorsearch")
 def doctorsearch():
@@ -177,6 +225,9 @@ def signup():
 def logout():
     logout_user()    
     return redirect("/")
+
+
+
 
 
 
